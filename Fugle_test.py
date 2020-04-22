b45b47bd0c1ffaaaabb89b46ae3386501a4847fc
Lars Peter Jørgensen <lars-games@outlook.com>
2020-04-22 12:32:22 +0200

    Co-authored-by: Zhinakin <malthebruhn129@gmail.com>

--- Fugle_test.py
+++ Fugle_test.py
@@ -1,9 +1,13 @@
 import sqlite3
 conn = sqlite3.connect("Fugle_db.db")
 c = conn.cursor()
+
 t = input("Type: ")
 att = input("Attribute: ")
-sql = "SELECT * FROM Fugl WHERE "+str(t)+"="+str(att)
+
+sql = ("SELECT Fugl FROM Fugle WHERE "+str(t)+" LIKE '"+str(att)+"'")
 print(sql)
 Fugle_var = c.execute(sql)
+Fugle_var = c.fetchall()
 print(Fugle_var)
+c.close()
